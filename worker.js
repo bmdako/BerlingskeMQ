280dd0a95ea3573eb79135a607e3688e36a4c23d
Daniel Jozie Kokott <dako@berlingskemedia.dk>
2014-06-03 11:35:33 +0200

    setup worker

--- worker.js
+++ worker.js
@@ -1,37 +1,69 @@
+/*jshint node: true */
+
+'use strict';
+
+var errors = [],
+    warnings = [];
+
 if (process.argv.length !== 3) {
-  console.log('Missing argument for worker id.')
+  errors.push(new Error('Missing argument for worker id.'));
+}
+
+if (process.env.REDIS_PORT === undefined) {
+  warnings.push('Environment variable REDIS_PORT missing');
+}
+
+if (process.env.REDIS_HOST === undefined) {
+  warnings.push('Environment variable REDIS_HOST missing');
+}
+
+
+if (warnings.length > 0) {
+  console.log('\Warnings:');
+  for (var i = warnings.length - 1; i >= 0; i--) {
+    console.log('  ' + warnings[i]);
+  }
+}
+
+if (errors.length > 0) {
+  console.log('\nErrors:');
+  for (var i = errors.length - 1; i >= 0; i--) {
+    console.log('  ' + errors[i]);
+  }
   process.exit(1);
 }
 
-var terminate = false;
+var eventEmitter = require('events').EventEmitter,
+    ee = new eventEmitter,
+    redis = require("redis"),
+    work_queue = 'work',
+    os = require('os'),
+    processing_queue = os.hostname() + '-' + process.argv[2],
+    currentTask = null,
+    terminate = false;
+
 process.on('SIGINT', function() {
   if (terminate) process.exit(0); // If pressed twice.
   console.log('Got SIGINT.  Please wait while the current task finished.');
   terminate = true;
 });
 
-var eventEmitter = require('events').EventEmitter,
-    ee = new eventEmitter,
-    redis = require("redis"),
-    client = redis.createClient(),
-    //redis.createClient(port, host, options)
-    work_queue = 'work',
-    os = require('os'),
-    workerid = processing_queue = os.hostname() + '-' + process.argv[2],
-    timeout = 0;
+ee.on('task_found', processTask);
+ee.on('task_processed', removeTask);
+ee.on('task_finished', nextTask);
+
+
+var client = process.env.REDIS_PORT && process.env.REDIS_HOST ?
+    redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST) :
+    redis.createClient();
+
 
 console.log('Starting worker ' + processing_queue);
-
-var currentTask;
-ee.on('queue_finished', nextTask);
-ee.on('task_found', processTask);
-ee.on('task_finished', removeTask);
-ee.on('task_removed', nextTask);
-nextTask();
+nextTask(); // Start
 
 
 function nextTask () {
-  client.lrange(processing_queue, 0, 0, function (err, tasks) {
+  client.lrange (processing_queue, 0, 0, function (err, tasks) {
     if ( tasks.length > 0 ) {
       currentTask = tasks[0];
       console.log('Resumed task: ' + currentTask);
@@ -41,7 +73,7 @@ function nextTask () {
       process.exit(0);
     } else {
       console.log('Wating for new task on ' + work_queue);
-      client.brpoplpush (work_queue, processing_queue, timeout, function (err, task) {
+      client.brpoplpush (work_queue, processing_queue, 0, function (err, task) {
         if (task) { // In case timeout is set to a value
           currentTask = task;
           console.log('Picked task: ' + currentTask);
@@ -60,8 +92,7 @@ function processTask () {
     process.exit(1);
   }
   setTimeout(function() {
-    console.log('Processed task: ' + currentTask);
-    ee.emit('task_finished');
+    ee.emit('task_processed');
   }, randomIntFromInterval(1000, 6000));
 }
 
@@ -70,8 +101,9 @@ function removeTask () {
     if (err || result !== 1) {
       throw new Error('Error when LREM from processing_queue ' + processing_queue);
     } else {
+      console.log('Finished task: ' + currentTask);
       currentTask = null;
-      ee.emit('task_removed');
+      ee.emit('task_finished');
     }
   });
 }
